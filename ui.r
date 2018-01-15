b8bd0b90871731bb577ec0513de352404b3f72e2
nakotaco <olenakotaco@gmail.com>
2018-01-14 21:39:26 -0500

    Create ui.r

---
+++ ui.r
@@ -0,0 +1,17 @@
+shinyUI(
+  ui = fluidPage(
+    selectInput("variable", "Variable:",
+                c("Cylinders" = "cyl",
+                  "Transmission" = "am",
+                  "Gears" = "gear")),
+    tableOutput("data")
+  )
+
+
+  selectInput(inputId = "n_breaks",
+              label = "Number of bins in histogram (approximate):",
+              choices = c(10, 20, 35, 50),
+              selected = 20),
+              
+              
+       )
