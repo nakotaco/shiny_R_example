3344bcbe419432d59cc686b4287d4f77dcc2ec37
nakotaco <olenakotaco@gmail.com>
2018-01-14 21:42:27 -0500

    Update ui.r

--- ui.r
+++ ui.r
@@ -1,17 +1,29 @@
-shinyUI(
-  ui = fluidPage(
-    selectInput("variable", "Variable:",
-                c("Cylinders" = "cyl",
-                  "Transmission" = "am",
-                  "Gears" = "gear")),
-    tableOutput("data")
+library(shiny)
+library(ggplot2)
+
+dataset <- diamonds
+
+pageWithSidebar(
+
+  headerPanel("Diamonds Explorer"),
+  
+  sidebarPanel(
+
+    sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
+                value=min(1000, nrow(dataset)), step=500, round=0),
+    
+    selectInput('x', 'X', names(dataset)),
+    selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
+    selectInput('color', 'Color', c('None', names(dataset))),
+    
+    checkboxInput('jitter', 'Jitter'),
+    checkboxInput('smooth', 'Smooth'),
+    
+    selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
+    selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
+  ),
+
+  mainPanel(
+    plotOutput('plot')
   )
-
-
-  selectInput(inputId = "n_breaks",
-              label = "Number of bins in histogram (approximate):",
-              choices = c(10, 20, 35, 50),
-              selected = 20),
-              
-              
-       )
+)
